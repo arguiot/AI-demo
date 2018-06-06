198c37da7b4f5da56e5bf5e37dc1360769477f0b
Arthur Guiot <arguiot@gmail.com>
2018-06-06 19:13:25 +0800

    Created files, and coded the app

--- AI Demo/ViewController.swift
+++ AI Demo/ViewController.swift
@@ -7,19 +7,134 @@
 //
 
 import UIKit
+import Vision
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    // video capture session
+    let session = AVCaptureSession()
+    // preview layer
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    // queue for processing video frames
+    let captureQueue = DispatchQueue(label: "captureQueue")
+    // overlay layer
+    var gradientLayer: CAGradientLayer!
+    // vision request
+    var visionRequests = [VNRequest]()
+    var recognitionThreshold : Float = 0.25
 
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var previewView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // get hold of the default video camera
+        guard let camera = AVCaptureDevice.default(for: .video) else {
+            fatalError("No video camera available")
+        }
+        
+        do {
+            
+            // add the preview layer
+            previewLayer = AVCaptureVideoPreviewLayer(session: session)
+            self.previewView.layer.addSublayer(previewLayer)
+            // add a slight gradient overlay so we can read the results easily
+            gradientLayer = CAGradientLayer()
+            gradientLayer.colors = [
+                UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.7).cgColor,
+                UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.0).cgColor,
+            ]
+            gradientLayer.locations = [0.0, 0.3]
+            self.previewView.layer.addSublayer(gradientLayer)
+            
+            // create the capture input and the video output
+            let cameraInput = try AVCaptureDeviceInput(device: camera)
+            
+            let videoOutput = AVCaptureVideoDataOutput()
+            videoOutput.setSampleBufferDelegate(self, queue: captureQueue)
+            videoOutput.alwaysDiscardsLateVideoFrames = true
+            videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+            session.sessionPreset = .high
+            
+            // wire up the session
+            session.addInput(cameraInput)
+            session.addOutput(videoOutput)
+            
+            // make sure we are in portrait mode
+            let conn = videoOutput.connection(with: .video)
+            conn?.videoOrientation = .portrait
+            
+            // Start the session
+            session.startRunning()
+            
+            
+            // set up the vision model
+            guard let model = try? VNCoreMLModel(for: SqueezeNet().model) else {
+                fatalError("Could not load model")
+            }
+            // set up the request using our vision model
+            let classificationRequest = VNCoreMLRequest(model: model, completionHandler: handleClassification)
+            classificationRequest.imageCropAndScaleOption = .centerCrop
+            visionRequests = [classificationRequest]
+        } catch {
+            print("Error")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        connection.videoOrientation = .portrait
+        
+        var requestOptions:[VNImageOption: Any] = [:]
+        
+        if let cameraIntrinsicData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
+            requestOptions = [.cameraIntrinsics: cameraIntrinsicData]
+        }
+        
+        // for orientation see kCGImagePropertyOrientation
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .upMirrored, options: requestOptions)
+        do {
+            try imageRequestHandler.perform(self.visionRequests)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func handleClassification(request: VNRequest, error: Error?) {
+        if let theError = error {
+            print("Error: \(theError.localizedDescription)")
+            return
+        }
+        guard let observations = request.results else {
+            print("No results")
+            return
+        }
+        
+        let classifications = observations[0...4]
+            .compactMap({ $0 as? VNClassificationObservation })
+        let max = classifications.max { a, b in a.confidence < b.confidence }
+        DispatchQueue.main.async {
+            self.label.text = max?.identifier
+        }
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer.frame = self.previewView.bounds;
+        gradientLayer.frame = self.previewView.bounds;
+        
+        previewLayer?.connection?.videoOrientation = .portrait
+    }
 }
 
